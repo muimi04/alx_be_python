40dc7ca5cc9888ca0016b835e4de2f8f7650d6c8
muimi04 <matthewmusili2@gmail.com>
2025-06-12 14:05:22 +0300

    fix: conform to all temp_conversion_tool checker requirements

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,15 +1,13 @@
+
 # temp_conversion_tool.py
 
-# Define global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
-    """Convert Fahrenheit to Celsius using global conversion factor."""
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    """Convert Celsius to Fahrenheit using global conversion factor."""
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
@@ -32,6 +30,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
 
 
 
